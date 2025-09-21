adeb853a10d013a528da0d5cc51552276fa92257
Alexander Rudenko <learnitnow@yandex.ru>
2025-09-21 11:49:01 +0300

    feat(core): first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
+import { AppProps } from "next/app";
+import CssBaseline from '@mui/material/CssBaseline';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    <CssBaseline />
+    <ChakraProvider value={defaultSystem}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ChakraProvider>
+    </>
+  )
+}
